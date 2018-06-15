c58c7ea362c08abbbeddbf3366aa59ae33faa136
AndrewTrahan <globalART19@gmail.com>
2018-06-15 10:54:45 -0500

    its working again

--- client/routes.js
+++ client/routes.js
@@ -4,7 +4,6 @@ import { withRouter, Route, Switch } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { Login, Signup, UserHome, LandingPage, HistoricalData } from './components'
 import { me } from './store'
-import Tfjs from './tfjs'
 
 class Routes extends Component {
   componentDidMount() {
