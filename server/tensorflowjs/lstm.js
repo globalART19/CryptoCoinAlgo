f1c4902a52b3bd32fabfffdc4f72d1347379f82e
Andrew Trahan <globalart19@gmail.com>
2018-06-14 22:50:39 -0500

    got it working on desktop finally

--- server/tensorflowjs/lstm.js
+++ server/tensorflowjs/lstm.js
@@ -1,10 +1,7 @@
-const tf = require('@tensorflow/tfjs');
+const tf = require("@tensorflow/tfjs");
 
 // Load the binding:
-require('@tensorflow/tfjs-node');  // Use '@tensorflow/tfjs-node-gpu' if running with GPU.
-
-// Set the backend to TensorFlow:
-tf.setBackend('tensorflow');
+require("@tensorflow/tfjs-node"); // Use '@tensorflow/tfjs-node-gpu' if running with GPU.
 
 const batchSize = 100;
 const lstmSize = 100;
@@ -12,11 +9,13 @@ const lstmSize = 100;
 //let lstm = tf.contrib.rnn.BasicLSTMCall(lstmSize)
 
 const lstmKernelArr = Array.apply(null, { length: 128 }).map(() => {
-  return Array.apply(null, { length: 10 }).map(() => Math.random() / 100)
-})
-const lstmBiasArr = Array.apply(null, { length: 10 }).map(() => Math.random() / 100)
+  return Array.apply(null, { length: 10 }).map(() => Math.random() / 100);
+});
+const lstmBiasArr = Array.apply(null, { length: 10 }).map(
+  () => Math.random() / 100
+);
 
-let lstmKernel = tf.tensor2d(lstmKernelArr)
-let lstmBias = tf.zeroes([10])
+let lstmKernel = tf.tensor2d(lstmKernelArr);
+let lstmBias = tf.tensor1d(Array.apply(null, { length: 10 }).map(() => 0));
 
-console.log(lstmBias)
+console.log(lstmBias);
